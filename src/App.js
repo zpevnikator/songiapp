bb6fd482cd22c6980180f77073c89d03ebdf3705
Jan Prochazka <jan.prochazka@gmail.com>
2023-10-27 08:36:09 +0200

    initial import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  async function loadSong() {
+    const resp = await fetch(
+      // "https://www.velkyzpevnik.cz/marien/a-bylo-leto"
+      // "https://www.riverlog.info/api/diary-info/stats?accountId=google:116106201198693582561&fromTm=1695620453830&apikey=certaky"
+      "https://raw.githubusercontent.com/dbgate/dbgate/master/app/src/electron.js"
+    );
+    console.log(await resp.text());
+  }
+
+  useEffect(() => {
+    loadSong();
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
